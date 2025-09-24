84b9473226240c34d571bc73774656770b67013e
DouglasBagambe <douglasbagambe4@gmail.com>
2025-09-24 13:06:14 +0300

    tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,20 +6,16 @@ import { configVariable } from "hardhat/config";
 const config: HardhatUserConfig = {
   plugins: [hardhatToolboxViemPlugin],
   solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
+    compilers: [
+      {
         version: "0.8.28",
         settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
+          optimizer: { enabled: true, runs: 200 },
+          viaIR: true,
         },
       },
-    },
+    ],
+    overrides: {},
   },
   networks: {
     hardhatMainnet: {
